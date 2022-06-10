3a9b9089fa89af306686244c0b997ec8a0f704cb
Dustin Huntsberger <dhunts258@gmail.com>
2022-06-10 11:26:01 -0400

    added paginaion support

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 #import all modules for project
-from urllib import response
+from os import times
 import requests
 from PIL import Image 
-import keys
+from keys import *
 import json
 import datetime
 
@@ -12,29 +12,40 @@ url = "https://api.samsara.com/fleet/documents?"
 # headers
 headers = {
     "Accept": "application/json",
-    "Authorization": "Bearer " + keys.SAMSARA_API_KEY
+    "Authorization": "Bearer " + SAMSARA_API_KEY
 }
 
 endTime = datetime.datetime.now()
 startTime = endTime - datetime.timedelta(1)
 
-urlTime = "endTime=" + endTime.strftime('%Y-%m-%dT04:00:00Z') + "&startTime=" + startTime.strftime('%Y-%m-%dT04:00:00Z')
+urlTime = "endTime=" + endTime.strftime('%Y-%m-%dT04:00:00Z') + "&startTime=" + startTime.strftime('%Y-%m-%dT04:00:00Z')    
 
-def api_call(URL):
+def processDocuments(documentId, endCursor):
+    if endCursor is not None:
+        URL = url + urlTime + "&documentTypeId=" + documentId + "&endCursor=" + endCursor
+    else:
+        URL = url + urlTime + "&documentTypeId=" + documentId
+
     response = requests.get(URL, headers=headers)
 
-    return json.loads(response.text)
+    parsedResponse = json.loads(response.text)
 
-parsedResponse = api_call(url + urlTime)
+    for index, document in enumerate(parsedResponse["data"]):
+        for field in document["fields"]:
+            try:
+                imageUrl = field["value"]["scannedDocumentValue"][0]["url"]
+                responseImage = requests.get(imageUrl)
+                timeString = datetime.datetime.now().strftime('%H%M%S%f')
+                open('output\document_' + timeString + str(index) + '.jpg', 'wb').write(responseImage.content)
+                test_1 = Image.open(r'output\document_'+ timeString + str(index) + '.jpg')
+                im_1 = test_1.convert('RGB')
+                im_1.save(r'output\processed\pdf_' + timeString + str(index) + '.pdf')
+            except KeyError:
+                print("")
+    
+    if(parsedResponse["pagination"]["hasNextPage"]):
+        processDocuments(documentId, parsedResponse["pagination"]["endCursor"])
 
-for index, document in enumerate(parsedResponse["data"]):
-    for field in document["fields"]:
-        try:
-            imageUrl = field["value"]["scannedDocumentValue"][0]["url"]
-            responseImage = requests.get(imageUrl)
-            open('output\document_' + str(index) + '.jpg', 'wb').write(responseImage.content)
-            test_1 = Image.open(r'output\document_' + str(index) + '.jpg')
-            im_1 = test_1.convert('RGB')
-            im_1.save(r'output\processed\pdf_' + str(index) + '.pdf')
-        except KeyError:
-            print("")
+processDocuments(billOfLadingId, None)
+# processDocuments(sendPaperworkId)
+# processDocuments(departDeliveryId)
